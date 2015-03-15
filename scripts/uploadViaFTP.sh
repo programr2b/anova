6379654aeb9e1686477e9ea06886258a675787ec
Victor Garcia <victor.garcia@araxid.com>
2015-03-15 19:05:18 -0400

    switching to root folder for production use

--- scripts/uploadViaFTP.sh
+++ scripts/uploadViaFTP.sh
@@ -2,5 +2,5 @@
 set -e # halt script on error
 
 cd ./_site
-find . -type f -exec curl -u $ANOVA_USER:$ANOVA_PASSWORD --ftp-create-dirs -T {} ftp://aikido-nova.org/htdocs/jekyll/{} \;
+find . -type f -exec curl -u $ANOVA_USER:$ANOVA_PASSWORD --ftp-create-dirs -T {} ftp://aikido-nova.org/htdocs/{} \;
 #find . -type f -exec ls {} \;
